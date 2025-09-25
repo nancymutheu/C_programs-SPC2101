b0deef9de983e415e083c7edb6b29742f285b565
nancymutheu <nancymutheu2007@gmail.com>
2025-09-25 10:40:54 +0300

    Add files via upload

---
+++ UNITS.C
@@ -0,0 +1,19 @@
+/*
+NAME:NANCY MUTHEU
+REG NO:PA106\G\29212\25
+DESCRIPTION: Program to display all units for this semester
+*/
+#include<stdio.h>
+
+int main()
+{	
+printf(" SPC 2101  Introduction to programming and problem solving\n");
+printf("SPI 2102   Computer system and organisation\n");
+printf(" SPM 2100 Discrete Mathematics\n");
+printf("SPM 2104  Matmetics for science\n");
+printf("SBE 2101 Fundamentals of system analysis annd design\n");
+printf("UCU 2105 Fundamntlas of ICT\n");
+printf("UCU 2106 Communication and information literacy skills\n");
+
+	return 0;
+}
