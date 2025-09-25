2531fde49b02a192c31dd303e981a2dc797a39e8
nancymutheu <nancymutheu2007@gmail.com>
2025-09-25 11:29:08 +0300

    Add files via upload

---
+++ height ID bank balance.c
@@ -0,0 +1,27 @@
+/*
+NAME:NANCY MUTHEU
+REG NO:PA106\G\29212\25
+DESCRIPTION: Program to display my height , ID number and Bank balance
+*/
+#include<stdio.h>
+
+int main()
+{
+	int HEIGHT;
+	printf("Enter your height");
+	scanf("%d", & HEIGHT);
+printf("your height is %d", HEIGHT);
+
+	int ID;
+		printf("Enter your ID number");
+	scanf("%d", & ID );
+	printf ("your ID number is %d" ,ID);
+	
+		int Bankbalance;
+		printf("Enter your bank balance");
+	scanf("%d", & Bankbalance);
+printf("your bank balance is %d" , Bankbalance);
+	return 0;
+}
+
+
