77f64c85e32000082c1ac9493180d99093f427bb
nancymutheu <nancymutheu2007@gmail.com>
2025-11-12 17:28:06 +0300

    Add files via upload

---
+++ Room Occupancy Management.c
@@ -0,0 +1,26 @@
+//Group B coding assignment 
+//NANCY MUTHEU
+//PA106/G/29212/25
+/* A Program to automatically declare whether room is vacant or occupied 10rooms in 5 floors*/
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+ 
+
+int main() {
+    int occupancy[5][10];
+    srand(time(0));
+
+    for (int f = 0; f < 5; f++) {
+        printf("floor %d\n",f+1);
+        for (int r = 0; r < 10; r++) {
+        printf("     room%-3d:",r+1);
+            if (occupancy[f][r]=rand()%2){
+             printf("occupied\n");}
+            else{
+             printf("vacant\n");}
+        }
+        }
+
+    return 0;
+}
