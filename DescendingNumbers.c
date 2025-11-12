5da9f939f4472e6fcac1c843b50957eedf30853b
nancymutheu <nancymutheu2007@gmail.com>
2025-11-12 17:11:16 +0300

    Add files via upload

---
+++ DescendingNumbers.c
@@ -0,0 +1,13 @@
+//Group B coding assignment 
+//NANCY MUTHEU
+//PA106/G/29212/25
+/* A PROGRAMME THAT PRINTS ALL NUMBERS 
+ FROM 100-50 IN DESCENDING ORDER USING FOR LOOP IN A NEW LINE*/
+#include <stdio.h>
+
+int main()
+{
+int x;
+for(x=100;x>49;x--)
+ printf("%d\n",x);
+}
